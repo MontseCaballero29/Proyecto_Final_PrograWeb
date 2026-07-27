71b0642873fa8dd19eb1e297dfe1a4b79c9e3851
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-27 01:44:59 -0600

    repositorio artesano con paginacion Closes #26

---
+++ backend/src/main/java/com/manosdeoaxaca/backend/repositorio/ArtesanoRepositorio.java
@@ -0,0 +1,14 @@
+package com.manosdeoaxaca.backend.repositorio;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.manosdeoaxaca.backend.model.Artesano;
+
+public interface ArtesanoRepositorio extends JpaRepository<Artesano, Long> {
+
+    Page<Artesano> findByComunidadId(Long comunidadId, Pageable pageable);
+
+    Page<Artesano> findByEstadoValidacion(String estadoValidacion, Pageable pageable);
+}
