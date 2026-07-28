d8d6a1b37e13768d8691fd0516d8e0d52169ef2d
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-27 23:29:17 -0600

    V91__insertar_artesano closes #32

---
+++ backend/src/main/resources/db/migration/V91__insertar_artesano.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuario (nombre, correo, password_hash, rol_id)
+VALUES (
+  'Artesano de Prueba',
+  'artesano@aripo.gob.mx',
+  '$2a$10$kGdwbSlaY0cDvTApmkp/HeZbjhxKyLpzMEyA1OlqC6wfUQbPfQOMq',
+  2
+);
