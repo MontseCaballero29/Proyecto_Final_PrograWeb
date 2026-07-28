239b1755581e4a99d3abc8f50bb618b9d64b0060
MeliGandarillas <gandarillasmelissa@gmail.com>
2026-07-27 23:16:14 -0600

    V90__insertar_admin

---
+++ backend/src/main/resources/db/migration/V90__insertar_admin.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuario (nombre, correo, password_hash, rol_id)
+VALUES (
+  'Administrador ARIPO',
+  'admin@aripo.gob.mx',
+  '$2a$10$7.40XQZypFtwqFudMunCp.q4oR5PeDvj0XDnflmZp4JtYPytQTFtK',
+  1
+);
